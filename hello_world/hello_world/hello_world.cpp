2c92def9845b9b5bfe39a10d9a145d2c0b58ae37
Miguel Arias <miguel_arias@csu.fullerton.edu>
2012-11-28 13:47:07 -0800

    add addition function

--- hello_world/hello_world/hello_world.cpp
+++ hello_world/hello_world/hello_world.cpp
@@ -2,9 +2,27 @@
 
 using namespace std;
 
+int add(int x, int y);
+
 int main ()
 {
 	cout << "Goodbye world!" << endl;
 
+	int a = 0;
+	int b = 0;
+
+	cout << "Enter first number" << endl;
+	cin >> a;
+	cout << "Enter second number" << endl;
+	cin >> b;
+	cout << "Sum is: " << add(a, b) << endl;
+
 	return 0;
+}
+
+int add(int x, int y)
+{
+	int sum = 0;
+	sum = x + y;
+	return sum;
 }
